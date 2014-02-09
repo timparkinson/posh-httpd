c72f29fcceda0d276ad2166ad0dfd73e95ec56c3
Tim Parkinson <t.r.parkinson@sheffield.ac.uk>
2014-02-09 10:18:01 +0000

    Starting afresh after mucho experimentation

--- Listener.Tests.ps1
+++ Listener.Tests.ps1
@@ -1,119 +1,31 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
-. "$here\helpers.ps1"
 
-# Pester does not appear to mock .NET objects and their methods currently, so no obvious tests for the core component: Invoke-HTTPListener
+Describe "Add-HTTPListener" {
+    $prefix = 'http://someprefix:8080/'
+    $scriptblock = {'blah'}
 
-Describe "Start-HTTPD" {
-    $non_existent_prefix = 'http://+:8080/qazwsxedcrfvtgbyhnujmikolp'
-    $content = {'blah'}
-    
-    Context "when called as a non admin or without an existing prefix" {
-        It "should throw an error" {
-            {Start-HTTPD -Prefix $non_existent_prefix -Content $content} | Should Throw
-        }
-    }
-
-    Context "when called with an already running prefix" {
-        Mock Test-HTTPDPrefix {$true}
+    Context "when called with a valid, existing prefix" {
         Mock Test-URLPrefix {$true}
+        Mock Initialize-HTTPDRunspace {}
 
-        It "Should throw an error" {
-            {Start-HTTPD -Prefix $non_existent_prefix -Content $content} | Should Throw
+        Add-HTTPListener -Prefix $prefix -scriptblock $scriptblock
+
+        It "should call Test-URLPrefix" {
+            Assert-MockCalled -commandName Test-URLPrefix
+        }
+
+        It "should call Initialize-HTTPRunspace" {
+            Assert-MockCalled -commandName
         }
     }
 
-    Context "when called with a non-running prefix" {
-        $mock_job = 'JOB!'
+    Context "when called with a non-existent prefix" {
+        Mock Test-URLPrefix {$false}
 
-        Mock Test-HTTPDPrefix {$false}
-        Mock Test-URLPrefix {$true}
-        Mock Start-Job {$mock_job}
-
-        Start-HTTPD -Prefix $non_existent_prefix -Content $content
-
-        It "Should set the running HTTPD prefix variable" {
-            $Script:HTTPDPrefixes.$non_existent_prefix | Should Not BeNullOrEmpty
-        }
-
-        $Script:HTTPDPrefixes.remove($non_existent_prefix)
-    }
-
-}
-
-Describe "Test-HTTPDPrefix" {
-    $prefix = 'http://+:8080/working'
-    
-
-    Context "when the HTTPD for a prefix is running" {
-        if (-not (Test-Path -Path VARIABLE:SCRIPT:HTTPDPrefixes)) {
-            $Script:HTTPDPrefixes = @{}
-        }
-
-        $Script:HTTPDPrefixes.Add($prefix, 'blah')
-
-        $result = Test-HTTPDPrefix -Prefix $prefix
-
-        It "should return $true" {
-            $result | Should Be $true
-        }
-    }
-
-    Context "when the HTTPD for a prefix is not running" {
-        try {
-            $Script:HTTPDPrefixes.Remove($prefix) 
-        } catch {}
-
-        $result = Test-HTTPDPrefix -Prefix $prefix
-
-        It "should return $false" {
-            $result | Should Be $false
-        }
-    }
-}
-
-Describe "Stop-HTTPD" {
-    $non_existent_prefix = 'http://+:8080/qazwsxedcrfvtgbyhnujmikolp'
-    $existing_prefix = 'http://+:8080/working'
-
-    if (-not (Test-Path -Path VARIABLE:SCRIPT:HTTPDPrefixes)) {
-            $Script:HTTPDPrefixes = @{}
-    }
-
-    $Script:HTTPDPrefixes.Add($existing_prefix, (new-object System.Management.Automation.ContainerParentJob 'blah'))
-    
-    Context "when called with a non-existant prefix" {
         It "should throw an error" {
-            {Stop-HTTPD -Prefix $non_existent_prefix} | Should Throw
+            {Add-HTTPListener} | Should Throw
         }
     }
-
-    Context "when called with an existing prefix" {
-        Mock Stop-Job -verifiable  {$true}
-
-        Stop-HTTPD -Prefix $existing_prefix
-
-        It "should remove the prefix" {
-            $Script:HTTPDPrefixes.containskey($existing_prefix) | Should Be $false
-        }
-
-        It "should call Stop-Job" {
-            Assert-VerifiableMocks
-        }
-    }
-}
-
-Describe "Restart-HTTPD" {
-    Mock Stop-HTTPD -verifiable {}
-    Mock Start-HTTPD -verifiable {}
-
-    Restart-HTTPD
-
-    Context "when called" {
-        It "should call the Stop/Start functions" {
-            Assert-VerifiableMocks
-        }
-    }
-
 }
--- posh-httpd.psm1
+++
@@ -1,6 +0,0 @@
-﻿# posh-httpd module
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-Get-ChildItem -Path "$here\*" -Include "*.ps1" -Exclude "*.Tests.ps1" |
-    ForEach-Object {
-        . $_.FullName
-    }
